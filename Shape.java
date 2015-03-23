a7bdee4e840fcfa1ec8af0cf441d5c8b175ae7ce
YuliiaGeraskina <julia.ulik@gmail.com>
2015-03-23 13:04:02 +0200

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,1 +1,22 @@
 
+import java.util.Comparator;
+
+public abstract class Shape implements IShapes, Comparable, Comparator {
+	protected String name;
+
+	public Shape() {
+
+	}
+
+	
+}
+class ShapeComparator implements Comparator<Object> {
+	@Override
+	public int compare(Object o1, Object o2) {
+		Shape h1 = (Shape)o1;
+		Shape h2 =(Shape)o2;
+		if (h1.getArea() > h2.getArea())	return 1;
+		else if (h1.getArea() < h2.getArea()) return -1;
+		return 0; 
+	}
+	}
